e9c19cd61b2805cfd766e7fe38ff9bb37b85c575
nhelz <nhelz@umich.edu>
2021-07-07 21:20:04 -0400

    Updated Bullet Base
    OnTriggerEnter2D now has a function that can be overriden. Added script for the shooting base script.

---
+++ Assets/Scripts/ShootBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBase : MonoBehaviour
+{
+    //Player has limited ammo, must reload to refill, otherwise unlimited.
+    //Temporary power up that changes how bullets fire and work (Player Exclusive)
+    //Bullet Pools (If useful, include for efficiency)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
