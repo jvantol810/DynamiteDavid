6fab7b994c9238a377726be2b0d9a23ff8c1d3ee
jvantol810 <63761346+jvantol810@users.noreply.github.com>
2021-07-13 20:46:11 -0400

    Refactored aim again

--- Assets/Scripts/Player/Aim.cs
+++ Assets/Scripts/Player/Aim.cs
@@ -21,14 +21,31 @@ public class Aim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        Vector2 reticleDir = mousePos.normalized;
-        reticlePos = m_rigidbody.position + reticleDir * reticleDistance;
-        reticle.transform.position = reticlePos;
+        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        //Vector2 reticleDir = mousePos.normalized;
+        //reticlePos = m_rigidbody.position + reticleDir * reticleDistance;
+        //reticle.transform.position = reticlePos;
 
-        Vector2 lookDirection = reticlePos;
-        float aimAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
-        m_rigidbody.rotation = aimAngle;
+        //Vector2 lookDirection = reticlePos;
+        //float aimAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        //m_rigidbody.rotation = aimAngle;
+
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        //Ta Daaa
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle+90));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
 
     public float getAimAngle()
