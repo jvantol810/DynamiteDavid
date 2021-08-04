a5bd6877b7669027c9aa65389e69dffcf884bbc7
CollinTift <collin.tift@gmail.com>
2021-08-03 20:36:40 -0400

    Added AI file for EMP

---
+++ Assets/Scripts/EMPEricAI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMPEricAI : MonoBehaviour
+{
+
+    public BossHealthUI bossHealthUI;
+    public int speed = 5;
+    public int health = 100;
+    public int maxHealth = 100;
+
+    private bool phase2 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
