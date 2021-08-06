f77a974604502819befa8cdfe49bf11a3d2af503
CollinTift <collin.tift@gmail.com>
2021-08-06 00:23:28 -0400

    Implemented IEntityStats and URP for EMP

--- Assets/Scripts/EMPEricAI.cs
+++ Assets/Scripts/EMPEricAI.cs
@@ -1,26 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
-public class EMPEricAI : MonoBehaviour
+public class EMPEricAI : MonoBehaviour, IEntityStats
 {
 
     public BossHealthUI bossHealthUI;
-    public int speed = 5;
-    public int health = 100;
-    public int maxHealth = 100;
 
-    private bool phase2 = false;
+    [Header("IEntityStats")]
+    [SerializeField]
+    public float _health;
+    [SerializeField]
+    public float _maxHealth;
+    public float health { get { return _health; } set { _health = health; } }
+    public float maxHealth { get { return _maxHealth; } set { _maxHealth = maxHealth; } }
+
+    [Header("Sprites")]
+    public SpriteRenderer spriteRenderer;
+    public Sprite spStage1;
+    public Sprite spStage2;
+    public Sprite spStage3;
+
+    [Header("Lights")]
+    public Light2D[] stageLights;
+
+    private bool midAction = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        bossHealthUI.SetMaxHealth(health);
+        bossHealthUI.SetCurrentHealth(health);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void takeDamage(float amt) 
+    {
+        if (health - amt <= 0) {
+            die();
+        }
+        health -= amt;
+    }
+
+    public void heal(float amt) 
+    {
+
+    }
+
+    public void die() 
+    {
+
     }
 }
